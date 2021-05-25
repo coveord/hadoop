5e8277ffeca15be8208dcb8ac222d551956f4f08
Alexis Cote <acote@coveo.com>
2021-05-25 15:50:01 -0400

    Deploy new version

--- build.sh
+++ build.sh
@@ -2,15 +2,18 @@
 COMMIT=$(git rev-parse HEAD | cut -c-8)
 NEW_VERSION=3.4.0-coveo-$COMMIT
 
-sed -i "s/<Xlint:-unchecked\/>//" ./hadoop-project/pom.xml
+#sed -i "s/<Xlint:-unchecked\/>//" ./hadoop-project/pom.xml
 mvn versions:set -DnewVersion="$NEW_VERSION"
 
 sed -i "s/<hadoop.version>\(.*\)<\/hadoop.version>/<hadoop.version>$NEW_VERSION<\/hadoop.version>/" pom.xml
-sed -i "s/<Xlint\/>/<Xlint\/><Xlint:-unchecked\/>/" ./hadoop-project/pom.xml
+#sed -i "s/<Xlint\/>/<Xlint\/><Xlint:-unchecked\/>/" ./hadoop-project/pom.xml
 
 DEPLOY_CMD="mvn -U deploy -DskipTests"
 
 mvn -U clean install -DskipTests
+
+# Deploy the main pom only
+mvn -N deploy -DskipTests
 (cd hadoop-project && $DEPLOY_CMD)
 (cd hadoop-project-dist && $DEPLOY_CMD)
 (cd hadoop-assemblies  && $DEPLOY_CMD)
